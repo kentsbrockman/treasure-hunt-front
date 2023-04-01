26a075cdf9fee3676d9986c21f88e915876b4e9e
Quentin Plaud <qplaud@gmail.com>
2023-04-01 19:22:56 +0200

    Gestion darkmode grille

--- src/style/Darkmode/Globalstyle.tsx
+++ src/style/Darkmode/Globalstyle.tsx
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
+    border-color: ${({ theme }) => theme.text};
     transition: all 0.50s linear;
   }
 `
