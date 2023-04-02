983ed2431b63dae02c1dcc9582c589aa70067fb7
Quentin Plaud <qplaud@gmail.com>
2023-04-02 11:20:41 +0200

    Ajout lib useEventCallback

--- @types/alltypes.d.ts
+++ @types/alltypes.d.ts
@@ -2,3 +2,4 @@ declare module "react-stepper-horizontal";
 declare module "react-form-stepper";
 declare module "assets/images/logo.png";
 declare module "assets/images/CEO.jpg";
+declare module "use-event-callback";
